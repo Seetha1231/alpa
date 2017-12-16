95e2cf4945b8b41ee0af6bd38063a8a124c0df85
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-16 12:27:14 +0530

    Create prim.py

---
+++ prim.py
@@ -0,0 +1,13 @@
+try:
+	n=int(input())
+	flag=0
+	for i in range(2,int(n/2)+1):
+		if n%i ==0:
+			flag=1
+			break
+	if flag==0:
+		print('prime')
+	else :
+		print('not prime')
+except:
+	print('invalid')
