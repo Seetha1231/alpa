9e920007b37f170db72370a6541c9254df29ef51
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-16 21:44:47 +0530

    Create big1.py

---
+++ big1.py
@@ -0,0 +1,19 @@
+def max():
+   	a=int(input())
+   	b=int (input())
+   	c=int(input())
+   	if (a>b):
+      if(a>c):
+    			print (a)
+   		else :
+    			print(c)
+   	elif (b>c):
+   		print (b)
+   	else:
+   		print (c)
+def main():
+   	try:
+    		max()
+   	except ValueError:
+    		print('invalid')
+main()
