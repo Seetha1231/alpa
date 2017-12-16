916d8a829ff7828a69d900698ae9ed6a580b0148
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-16 13:35:52 +0530

    Create pril.py

---
+++ pril.py
@@ -0,0 +1,14 @@
+try:
+	s=int(input())
+	e=int(input())
+	for j in range(s,e+1):
+		flag=0
+		for i in range(2,int(j/2)+1):
+			if j%i ==0:
+				flag=1
+				break
+		if flag==1:
+			continue
+		print(j)
+except:
+	print('invalid')
