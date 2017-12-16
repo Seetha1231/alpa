54525609d20e329cb54522e08e702ae337fb55e7
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-16 15:17:13 +0530

    Create rtoi.py

---
+++ rtoi.py
@@ -0,0 +1,15 @@
+def rom_to_int(string):
+    table=[['M',1000],['CM',900],['D',500],['CD',400],['C',100],['XC',90],['L',50],['XL',40],['X',10],['IX',9],['V',5],['IV',4],['I',1]]
+    returnint=0
+    for pair in table:
+    	continueyes=True
+        while continueyes:
+            if len(string)>=len(pair[0]):
+                if string[0:len(pair[0])]==pair[0]:
+                    returnint+=pair[1]
+                    string=string[len(pair[0]):]
+                else: continueyes=False
+            else: continueyes=False
+
+    return returnint
+rom=input()
