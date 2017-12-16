1b2aa52ce813849b73a010ead1fea5a1a4290dc4
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-16 22:27:15 +0530

    Create fibs1.py

---
+++ fibs1.py
@@ -0,0 +1,12 @@
+def fib(n):
+	if n==1 or n==0:
+		return(n)
+	else :
+		return fib(n-1)+fib(n-2)
+try:
+	n=int(input('Enter n :'))
+	sum=0
+	for i in range(0,n):
+		print(fib(i))
+except:
+print('invalid')
