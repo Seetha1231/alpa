3751c5e77d8fa3ad5c2e01556b5eb3d9bff15122
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-16 16:48:52 +0530

    Update fac.py

--- fac.py
+++ fac.py
@@ -1,8 +1,11 @@
 try:
 	f=1
 	n=int(input())
-	for i in range(1,n+1):
-		f*=i
-	print(f)
+	if(n>=0):
+		for i in range(1,n+1):
+			f*=i
+		print(f)
+	else:
+		print('please enter positive integer')
 except:
 	print('invalid')
