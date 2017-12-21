2ac0b03ce196be358bf01788db2e7bc7aeadd468
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-21 23:35:56 +0530

    Create strcmp.py

---
+++ strcmp.py
@@ -0,0 +1,15 @@
+def compare(str1,str2):
+	if str1==str2:
+		print(str2)
+	elif str1>str2:
+		print(str1)
+	else :
+		print(str2)
+def main():
+	try:
+		s1=input()
+		s2=input()
+		compare(s1,s2)
+	except:
+		print('nvalid')
+main()
