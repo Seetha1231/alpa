2c9fafa2ad90f33e0fd01683c8538aa31ff8b9d8
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-21 23:54:50 +0530

    Create maxmin.py

---
+++ maxmin.py
@@ -0,0 +1,17 @@
+def countd(l,n):
+	(max,min)=(0,9999)
+	for i in range(n):
+		if max<l[i]:
+			max=l[i]
+		if min>l[i]:
+			min=l[i]
+	print(max,min)
+def main():
+	try:
+		l=[]
+		n=int(input())
+		l.append(int(input()))
+		countd(l,n)
+	except:
+		print('invalid')
+main()
