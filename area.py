26ed3061dd59c9f8925a8b0b427a14d6f89b6b40
Seetha1231 <34570365+Seetha1231@users.noreply.github.com>
2017-12-25 12:21:04 +0530

    Update area.py

--- area.py
+++ area.py
@@ -10,13 +10,17 @@ def triangle(b,h):
 	area=0.5*b*h
 	print("Area  :",area)
 def main():
-	r=int(input("Enter radius for circle :\n"))
-	circle(r)
-	print("\n ----Rectangle----\n")
-	l=int(input("Enter length :\n"))
-	b=int(input("Enter breadth :\n"))
-	rectangle(l,b)
-	print("\n----Triangle---- \n")
-	b=int(input("\nEnter Breadth  :\n"))
-	h=int(input("Enter Height  :\n"))
-	triangle(b,h)
+	try:
+		r=int(input("Enter radius for circle :\n"))
+		circle(r)
+		print("\n ----Rectangle----\n")
+		l=int(input("Enter length :\n"))
+		b=int(input("Enter breadth :\n"))
+		rectangle(l,b)
+		print("\n----Triangle---- \n")
+		b=int(input("\nEnter Breadth  :\n"))
+		h=int(input("Enter Height  :\n"))
+		triangle(b,h)
+	except:
+		print('invalid')
+main()
